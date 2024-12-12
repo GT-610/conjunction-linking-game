a18f81b14019b21ca9acb9178ad481acf8395769
GT610 <myddz1005@163.com>
2024-12-12 14:30:21 +0800

    Use exist font for blocks & better hint algorithm

--- backend/block.py
+++ backend/block.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
 
-from frontend.commons import font_path, BLUE, WHITE, YELLOW, BLACK
+from frontend.commons import small_font, BLUE, WHITE, YELLOW, BLACK
 
 class Block:
     def __init__(self, map, point, block_size, offset_x, offset_y):
@@ -14,9 +14,6 @@ class Block:
         self.outerY = offset_y + self.innerX * block_size  # 外部 y 坐标
         self.outerCenterPoint = (self.outerX + int(self.outerWidth / 2), self.outerY + int(self.outerWidth / 2))
         self.rect = pygame.Rect(self.outerX, self.outerY, self.outerWidth, self.outerWidth)
-        
-        # 块内字体，显示 0 或 1
-        self.font = pygame.font.Font(font_path, 36)  
 
         # 选中状态
         self.selected = False
@@ -35,7 +32,7 @@ class Block:
         pygame.draw.rect(screen, BLACK, (self.outerX, self.outerY, self.outerWidth, self.outerWidth), 2)
 
         # 渲染块的值（0 或 1）
-        value_text = self.font.render(str(self.value), True, BLACK)
+        value_text = small_font.render(str(self.value), True, BLACK)
         value_rect = value_text.get_rect(center=(self.outerX + self.outerWidth // 2, self.outerY + self.outerWidth // 2))  # 计算中心坐标
         screen.blit(value_text, value_rect)
 
--- frontend/pages/loading.py
+++ frontend/pages/loading.py
@@ -5,6 +5,13 @@ pygame.init()
 # 显示加载进度的函数
 def display_loading_message(screen, message, font):
     screen.fill(BLACK)  # 清空屏幕
+
+    # 显示加载文本
+    map_generating_text = font.render("游戏加载中", True, WHITE)
+    map_generating_rect = map_generating_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 50))
+    screen.blit(map_generating_text, map_generating_rect)
+
+    # 显示加载项
     loading_text = font.render(message, True, WHITE)
     loading_rect = loading_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
     screen.blit(loading_text, loading_rect)
